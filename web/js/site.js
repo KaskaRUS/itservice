53cef0d1df393bd7fb909a841ad576a867ead09b
Nikolai Zhdanov <nikolai@zhdanov.space>
2018-03-02 11:29:58 +0300

    Add processing form

--- web/js/site.js
+++ web/js/site.js
@@ -14,17 +14,45 @@ $(document).ready(function() {
     });
 
 
-    $('a[href^="#"]').click(function() {
-        elementClick = $(this).attr("href").split("#");
-        destination = $("a[name=" + elementClick[1] + "]").offset().top;
-        $("html, body").animate({
-            scrollTop: destination
-        }, 800);
-        return false;
+    // $('a[href^="#"]').click(function() {
+    //     elementClick = $(this).attr("href").split("#");
+    //     destination = $("#" + elementClick[1]).offset().top;
+    //     $("html, body").animate({
+    //         scrollTop: destination
+    //     }, 800);
+    //     return false;
+    // });
+
+    $('#form_name, #form_email, #form_title, #form_message').change(function() {
+        $(this).removeClass("error");
     });
 
     $('#message_form').submit(function() {
         
+        if ($("#form_name").val() == "") {
+            $("#form_name").addClass("error");
+            $("#form_name").focus();
+            return false;
+        }
+
+        if ($("#form_email").val() == "") {
+            $("#form_email").addClass("error");
+            $("#form_email").focus();
+            return false;
+        }
+
+        if ($("#form_title").val() == "") {
+            $("#form_title").addClass("error");
+            $("#form_title").focus();
+            return false;
+        }
+
+        if ($("#form_message").val() == "") {
+            $("#form_message").addClass("error");
+            $("#form_message").focus();
+            return false;
+        }
+
         data = {
                 "name": $("#form_name").val(),
                 "email": $("#form_email").val(),
