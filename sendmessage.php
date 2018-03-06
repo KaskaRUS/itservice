f4cca5dda350d98b6e2088902974a64b8e707be9
Nikolai Zhdanov <nikolai@zhdanov.space>
2018-03-06 10:09:59 +0300

    fix script sending mail

--- sendmessage.php
+++ sendmessage.php
@@ -1,4 +1,7 @@
 <?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
 	mb_internal_encoding("UTF-8");
 	$email = $_POST["email"];
 	$name = $_POST["name"];
@@ -6,15 +9,42 @@
 	$message = $_POST["message"];
 	$phone = $_POST["phone"];
 
-	$to = "b2b@org-tech.ru";
-	if ($email == "")
-		$email = "noreply@org-tech.ru";
-	$message .= "\n От $name: $email; tel.: $phone";
-	$headers = "From: $name <$email>\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html; charset=utf-8";
+	$message .= "\n От $name: $email; $phone";
 
-	mail($to, $title, $message, $headers); 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 
-	echo "{result:true}";
+//Load composer's autoloader
+	require 'vendor/autoload.php';
+
+	$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+	try {
+	    //Server settings
+	    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+	    $mail->isSMTP();                                      // Set mailer to use SMTP
+	    $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+	    $mail->Username = 'noreply@org-tech.ru';                 // SMTP username
+	    $mail->Password = 'ZKw1{jOT';                           // SMTP password
+	    $mail->SMTPSecure = 'ssl';    
+	    $mail->CharSet = "utf-8";                        // Enable TLS encryption, `ssl` also accepted
+	    $mail->Port = 465;                                    // TCP port to connect to
+
+	    //Recipients
+	    $mail->setFrom('noreply@org-tech.ru', 'Site message');
+	    $mail->addAddress('b2b@org-tech.ru');     // Add a recipient
+
+	    //Content
+	    $mail->isHTML(false);                                  // Set email format to HTML
+	    $mail->Subject = $title;
+	    $mail->Body    = $message;
+
+	    $mail->send();
+	    echo "{result:true}";
+	} catch (Exception $e) {
+	    echo "{result:false}";
+	}
+
+
+	
 ?>
